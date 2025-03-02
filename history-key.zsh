6e4cea99f3f949116400ee5befcd1c9aeefd73a8
Tal Wrii <talwrii@gmail.com>
2025-03-02 15:12:14 +0100

    Make number of entries variable and fix order of output

--- history-key.zsh
+++ history-key.zsh
@@ -1,7 +1,7 @@
 here=$(dirname ${(%):-%x})
 
 history-key () {
-    command=$(history -n -1000 | $here/filter | $here/select)
+    command=$(history -n -1000 | $here/filter 10 | $here/select)
     LBUFFER=$command
     zle reset-prompt
 }
